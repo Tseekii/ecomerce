bf94d55713a64202584dbab4ac0a293eadf9bfb9
Tseekii <tseekiibatanu@gmail.com>
2024-09-17 12:41:27 +0800

    917

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState<number>(0);
+  const minus = () => {
+    setCount(count - 1);
+  };
+  const add = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="flex">
+      <h1>Welcome to e-comerce App</h1>
+      <Button onClick={minus}>-</Button>
+      <span>{count}</span>
+      <Button onClick={add}>+</Button>
+    </div>
+  );
+}
