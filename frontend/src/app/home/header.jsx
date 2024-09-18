371c18859221836e84de655d5c5dcbeb22cead38
Tseekii <tseekiibatanu@gmail.com>
2024-09-18 12:17:04 +0800

    918

---
+++ frontend/src/app/home/header.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useContext, useState } from "react";
+
+export const Header = () => {
+  return (
+    <header className="">
+      <div className="flex items-center gap-6">
+        <img src="/Logo.png" alt="" />
+        <span>Ангилал</span>
+        <button>Бүтээгдэхүүн хайх</button>
+        <img src="/Save.png" alt="" />
+        <img src="shopping-cart.png" alt="" />
+      </div>
+    </header>
+  );
+};
