e6a577c45e160a4f6148cb6ab8ff0826c634efc4
Tseekii <tseekiibatanu@gmail.com>
2024-10-04 12:45:06 +0800

    1004

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,21 +1,3 @@
-// "use client";
-
-// export const Header = () => {
-//   const { user } = useUser();
-//   console.log("user", user);
-
-//   return (
-//     <header className="">
-//       <div className="flex items-center gap-6">
-//         <img src="/Logo.png" alt="" />
-//         <span>Ангилал</span>
-//         <button>Бүтээгдэхүүн хайх</button>
-//         <img src="/Save.png" alt="" />
-//         <img src="shopping-cart.png" alt="" />
-//       </div>
-//     </header>
-//   );
-// };
 "use client";
 import { Heart, Search, ShoppingCart } from "lucide-react";
 import Image from "next/image";
